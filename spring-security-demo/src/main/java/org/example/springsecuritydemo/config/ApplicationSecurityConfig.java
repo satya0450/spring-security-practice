8ad3a6f8b88686fbfb35476e23940e29f4dc4e95
satyajanardhan <satyajanardhan@virtusa.com>
2026-02-14 08:05:44 +0530

    static files and spring security 6 related files are added due to extend websecurityconfigadapter is deprecated

--- spring-security-demo/src/main/java/org/example/springsecuritydemo/config/ApplicationSecurityConfig.java
+++ spring-security-demo/src/main/java/org/example/springsecuritydemo/config/ApplicationSecurityConfig.java
@@ -1,4 +1,28 @@
 package org.example.springsecuritydemo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+        .authorizeHttpRequests(
+                (authz) -> authz
+                .requestMatchers( "/","/index", "/css/*", "/js/*").permitAll()
+                .anyRequest()
+                .authenticated()
+        )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
 }
